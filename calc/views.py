92c1790dfb189d62b85ad052d479ccee1af4a59f
Arpit957 <arpittiwari2001@gmail.com>
2025-10-23 00:49:58 +0530

    Update index, models and views

--- calc/views.py
+++ calc/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Features
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html' ,{'name': 'fastybanno'})
+    feature1 = Features()
+    feature1.id = 1 
+    feature1.name = "Addition"
+    feature1.description = "Perform addition of two numbers."
+
+    feature2 = Features()
+    feature2.id = 2
+    feature2.name = "Subtraction"
+    feature2.description = "Perform subtraction of two numbers."
+    
+    features = [feature1, feature2]
+    return render(request, 'index.html', {'features': features})
 
 def result(request):
     num1 = int(request.POST['num1'])

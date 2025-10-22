33bd86116753bd8dd6d8dc1d7ad0e97cf1361881
Arpit957 <arpittiwari2001@gmail.com>
2025-10-23 01:21:59 +0530

    Update Admin, models and views

--- calc/views.py
+++ calc/views.py
@@ -5,17 +5,7 @@ from .models import Features
 # Create your views here.
 
 def home(request):
-    feature1 = Features()
-    feature1.id = 1 
-    feature1.name = "Addition"
-    feature1.description = "Perform addition of two numbers."
-
-    feature2 = Features()
-    feature2.id = 2
-    feature2.name = "Subtraction"
-    feature2.description = "Perform subtraction of two numbers."
-    
-    features = [feature1, feature2]
+    features = Features.objects.all()
     return render(request, 'index.html', {'features': features})
 
 def result(request):
